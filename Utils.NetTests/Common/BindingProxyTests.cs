ed41009d294592a9eda0359a6bd255a72fb9f98f
Hristo Kapanakov <hr.kapanakov@gmail.com>
2018-06-01 17:35:01 +0300

    Utils.NetTests * Add tests for BindingProxy, ValidationRule, RelativeFontSizeHelper * Freezable objects clone call for code coverage * Small fixes
    Run unit tests on deploy.

---
+++ Utils.NetTests/Common/BindingProxyTests.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Utils.Net.Common.Tests
+{
+    [TestClass]
+    public class BindingProxyTests
+    {
+        [TestMethod]
+        public void BindingProxyTest()
+        {
+            var testBindingProxy = new BindingProxy
+            {
+                Data = "test"
+            };
+
+            // for code coverage
+            testBindingProxy.Clone();
+
+            Assert.AreEqual(testBindingProxy.Data, "test");
+            Assert.IsInstanceOfType(testBindingProxy.As<string>(), typeof(string));
+        }
+    }
+}
